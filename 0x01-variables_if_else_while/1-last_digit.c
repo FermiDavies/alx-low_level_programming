432cec7ab0f2a2ed57a7e3487f9321b72b3ffae9
FermiDavies <faithafolabi1@gmail.com>
2022-06-03 15:32:58 -0700

    Variables and conditional statements

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main -Determine if n is greater than 5, less than 6 and not 0, is 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0)0;
+	n = rand() - RAND_MAX / 2;
+	b = n % 10;
+	if (b > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, b);
+	}
+	else if (b == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, b);
+	}
+	else
+	{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, b);
+	}
+	return (0);
+}
