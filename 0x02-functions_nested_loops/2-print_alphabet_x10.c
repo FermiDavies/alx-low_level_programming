21bddc553e4cb4836c8b41b879fd3ac3c9b529eb
FermiDavies <faithafolabi1@gmail.com>
2022-06-07 13:07:14 -0700

    Task done. It works.

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabets in 1-alphabet 10 times.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 9; x++ }
+		{
+			for (y = 97; y <= 122; y++)
+			{
+				_putchar(y);
+			}
+		_putchar('\n');
+		}
+}
