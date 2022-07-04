574aa27f165d626e345830763c083ff360cc519c
FermiDavies <faithafolabi1@gmail.com>
2022-07-04 15:10:02 -0700

    0x0E-structures_typedef tasks

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ *
+ * Return: struct dog.
+ * if fails, returns NULL.
+ */
+
